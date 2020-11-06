6093fa2a7c564f2ecb4914a6fbbdc6e5ff72e111
Saranoja <Saranoja@users.noreply.github.com>
2020-11-06 15:24:28 +0200

    fix: fixed redundance on conn occurence

--- node_B.py
+++ node_B.py
@@ -7,27 +7,27 @@ from AES_pack import *
 key, encrypted_key = b'', b''
 
 
-def get_mode_of_operation(con):
+def get_mode_of_operation():
     mode = conn.recv(3)
     print(f'Mode of operation: {mode.decode("utf-8")}')
     return mode
 
 
-def get_key_and_decrypt(con):
+def get_key_and_decrypt():
     global key, encrypted_key
-    encrypted_key = con.recv(16)
+    encrypted_key = conn.recv(16)
     print(f'Key from A: {encrypted_key}')
     key = AES_decrypt(encrypted_key, commons.K_prime)
     print(f'Decrypted key: {key}')
 
 
-def send_start_signal(con):
-    con.sendall(bytes("Start", "utf-8"))
+def send_start_signal():
+    conn.sendall(bytes("Start", "utf-8"))
 
 
-def get_message(con):
-    file_size = con.recv(4)
-    return con.recv(int(str(file_size, 'utf8')))
+def get_message():
+    file_size = conn.recv(4)
+    return conn.recv(int(str(file_size, 'utf8')))
 
 
 def decrypt_message_on_chosen_mode(mode, message):
@@ -45,12 +45,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, _ = s.accept()
     with conn:
-        mode_of_operation = get_mode_of_operation(conn)
+        mode_of_operation = get_mode_of_operation()
 
-        get_key_and_decrypt(conn)
+        get_key_and_decrypt()
 
-        send_start_signal(conn)
+        send_start_signal()
 
-        decrypted_message = decrypt_message_on_chosen_mode(mode_of_operation.decode('utf-8'), get_message(conn))
+        decrypted_message = decrypt_message_on_chosen_mode(mode_of_operation.decode('utf-8'), get_message())
 
         print(f'Message from A: {decrypted_message.decode("utf-8")}')
